481c446af4f5c3bc53693840c84229294a742338
Jhon Mena <jmena1719@gmail.com>
2025-06-29 16:54:07 -0500

    first commit

---
+++ src/main/java/com/service/userstore/mcs_motor_transferencia/McsMotorTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.service.userstore.mcs_motor_transferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsMotorTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsMotorTransferenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/userstore/mcs_motor_transferencia/McsMotorTransferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.userstore.mcs_motor_transferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsMotorTransferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
